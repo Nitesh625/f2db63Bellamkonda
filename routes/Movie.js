c039150442ca37c5e944dc26bc7e02401f70283e
Nitesh625 <bellamkondanitesh14@gmail.com>
2022-11-16 14:31:32 -0600

    updated

--- routes/Movie.js
+++ routes/Movie.js
@@ -1,13 +1,27 @@
 var express = require('express');
+var Movie_controller = require('../controllers/Movie');
 var router = express.Router();
 
+// router.get('/', function(req, res, next) 
+// {
+//   res.render('Movie', { title: 'Search Results Movie' });
+// });
 
+router.get('/', Movie_controller.Movie_view_all_Page ); 
 
-router.get('/', function(req, res, next) 
-{
-  res.render('Movie', { title: 'Search Results Movie' });
-});
+// GET request for one Movie. 
+router.get('/Movie/:id', Movie_controller.Movie_detail);
 
+/* GET detail Movie page */ 
+router.get('/detail', Movie_controller.Movie_view_one_Page); 
+
+/* GET create Movie page */ 
+router.get('/create', Movie_controller.Movie_create_Page); 
+
+/* GET create update page */ 
+router.get('/update', Movie_controller.Movie_update_Page);
+
+/* GET delete Movie page */ 
+router.get('/delete', Movie_controller.Movie_delete_Page);
 module.exports = router;
-
  
