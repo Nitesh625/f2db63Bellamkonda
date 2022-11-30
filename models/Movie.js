e671583d249382d9a282f31321cb379ed5836687
Nitesh625 <bellamkondanitesh14@gmail.com>
2022-11-30 11:35:20 -0600

    updated files

--- models/Movie.js
+++ models/Movie.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose") 
 const MovieSchema = mongoose.Schema({ 
- movieName: String, 
- movieType: String, 
- durMin: Number 
+ movieName: {type: String,required: [true, 'Name of the Movie cannot be empty']}, 
+ movieType: {type: String,required: [true, 'Movie type cannot be empty']}, 
+ durMin: {type: Number,required: [true, 'Movie duration cannot be empty']}
 }) 
  
 module.exports = mongoose.model("Movie", MovieSchema) 
--- routes/Movie.js
+++ routes/Movie.js
@@ -2,26 +2,24 @@ var express = require('express');
 var Movie_controller = require('../controllers/Movie');
 var router = express.Router();
 
-// router.get('/', function(req, res, next) 
-// {
-//   res.render('Movie', { title: 'Search Results Movie' });
-// });
-
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
 router.get('/', Movie_controller.Movie_view_all_Page ); 
-
 // GET request for one Movie. 
 router.get('/Movie/:id', Movie_controller.Movie_detail);
-
 /* GET detail Movie page */ 
-router.get('/detail', Movie_controller.Movie_view_one_Page); 
-
+router.get('/detail',secured, Movie_controller.Movie_view_one_Page); 
 /* GET create Movie page */ 
-router.get('/create', Movie_controller.Movie_create_Page); 
-
+router.get('/create',secured, Movie_controller.Movie_create_Page); 
 /* GET create update page */ 
-router.get('/update', Movie_controller.Movie_update_Page);
-
+router.get('/update',secured, Movie_controller.Movie_update_Page);
 /* GET delete Movie page */ 
-router.get('/delete', Movie_controller.Movie_delete_Page);
+router.get('/delete',secured, Movie_controller.Movie_delete_Page);
 module.exports = router;
  
